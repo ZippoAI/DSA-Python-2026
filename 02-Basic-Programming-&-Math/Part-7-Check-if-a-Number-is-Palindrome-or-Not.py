aa276ca7e8431fd307542f0976078a7629804467
ZippoAI <iamrealzippo@gmail.com>
2026-04-19 00:05:19 +0530

    part 7 done

--- 02-Basic-Programming-&-Math/Part-7-Check-if-a-Number-is-Palindrome-or-Not.py
+++ 02-Basic-Programming-&-Math/Part-7-Check-if-a-Number-is-Palindrome-or-Not.py
@@ -1,3 +1,23 @@
 # Part 7: Check if a Number is Palindrome or Not
 
 
+n = 567765
+num = n
+result = 0
+while num>0:
+    ld= num%10
+    result = result*10 + ld
+    num = num//10
+
+if result==n:
+    print('Palindrome')
+else:
+    print('Not palindrome')
+    
+
+
+
+
+
+
+
