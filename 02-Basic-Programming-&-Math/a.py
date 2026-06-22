f018a88907571b0a790e4457db79283b3b70a729
ZippoAI <iamrealzippo@gmail.com>
2026-06-22 23:42:23 +0530

    a

--- 02-Basic-Programming-&-Math/a.py
+++ 02-Basic-Programming-&-Math/a.py
@@ -1,17 +1,12 @@
 a = 12321
+
 num = a
+
 result = 0
+
 while num>0:
     ld = num%10
     result = result*10 + ld
     num = num//10
 
-ot = [True if result==a else False]
-
-
-print(ot)
-
-
-
-
-    
+print(result==a)
