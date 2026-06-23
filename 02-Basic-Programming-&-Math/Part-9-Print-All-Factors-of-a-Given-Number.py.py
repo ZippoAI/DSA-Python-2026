248f0fc51a2910858118e515b0893fc156e66422
ZippoAI <iamrealzippo@gmail.com>
2026-06-24 00:15:33 +0530

    a

--- 02-Basic-Programming-&-Math/Part-9-Print-All-Factors-of-a-Given-Number.py.py
+++ 02-Basic-Programming-&-Math/Part-9-Print-All-Factors-of-a-Given-Number.py.py
@@ -1,3 +1,5 @@
+# method 1
+
 n = 10
 
 num = n
@@ -9,6 +11,39 @@ for i in range(1, n+1):
         store.append(i)
     else:
         continue
-
+print()
+print('Brute Solution')
 print(store)
 
+ 
+# method 2
+
+n = 10
+
+num = n
+
+result = []
+
+for i in range(1, num//2+1):
+    if num%i==0:
+        result.append(i)
+result.append(num)
+print()
+print('Better Solution')
+print(result)
+
+
+# ------------------ METHOD 3------------------------
+from math import sqrt
+n = 36
+num = n
+result = []
+
+for i in range(1, int(sqrt(num))+1):
+    if num%i==0:
+        result.append(i)
+        if num//i !=i:
+            result.append(num//i)
+print()
+print('Optimal Solution')
+print(sorted(result))
